a926097ae947e7856f6f3852594e2253e3de1d70
Juan Gutierrez <juanny.gee@gmail.com>
2024-10-26 08:35:25 -0400

    Add commands to access clean & coverage `forge` commands
    Extra:
    * Run contract unit tests + coverage report in github action

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     Spotlight: {
-      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+      address: "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0",
       abi: [
         {
           type: "constructor",
