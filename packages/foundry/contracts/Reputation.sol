53180511af2b9c5f996f42bd5207f249f4fed50b
tianqizhao-louis <louiszhao0517@gmail.com>
2024-10-27 01:30:24 -0400

    Symbol

--- packages/foundry/contracts/Reputation.sol
+++ packages/foundry/contracts/Reputation.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Reputation is ERC20 {
-  constructor() ERC20("Reputation", "DVT") {
+  constructor() ERC20("Reputation", "RPT") {
     _mint(msg.sender, 1000 * 10 ** 18);
   }
 }
