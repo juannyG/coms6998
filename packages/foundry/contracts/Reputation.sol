677ea620634d5e0536942e6f3c1cab6a46ba6675
tianqizhao-louis <louiszhao0517@gmail.com>
2024-10-27 01:14:05 -0400

    erc20 token exprimentation

---
+++ packages/foundry/contracts/Reputation.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Reputation is ERC20 {
+  constructor() ERC20("Reputation", "DVT") {
+    _mint(msg.sender, 1000 * 10 ** 18);
+  }
+}
