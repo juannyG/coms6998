abeb249ba68aff297626af8cf97bf519c9f2bf7b
Hung-Ching Liu <b05902043@ntu.edu.tw>
2024-11-02 02:40:00 -0400

    chore: lint

--- packages/nextjs/app/feed/richTextEditor/plugins/RestoreContentFromData.js
+++ packages/nextjs/app/feed/richTextEditor/plugins/RestoreContentFromData.js
@@ -1,4 +1,4 @@
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 
