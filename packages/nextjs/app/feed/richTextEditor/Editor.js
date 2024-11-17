59321678095db6a44908af0c20ac4c8f61c7a846
Juan Gutierrez <juanny.gee@gmail.com>
2024-11-17 08:57:14 -0500

    Lint checks

--- packages/nextjs/app/feed/richTextEditor/Editor.js
+++ packages/nextjs/app/feed/richTextEditor/Editor.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { EditorContext } from "../context";
 import "../richTextEditor/styles.css";
 import AutoLinkPlugin from "./plugins/AutoLinkPlugin";
@@ -52,7 +52,7 @@ const editorConfig = {
 };
 
 export default function Editor() {
-  const { confirmPost, confirmPaywallPost } = useContext(EditorContext);
+  const { confirmPost } = useContext(EditorContext);
   const { paywallSupported } = useContext(PaywallSupportContext);
 
   return (
@@ -76,14 +76,14 @@ export default function Editor() {
           <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
         </div>
         <div className="flex justify-end pt-2">
-          <button className="btn btn-outline rounded text-[#3466f6] border border-[#3466f6]" onClick={async(e) => confirmPost(e, false) }>
+          <button className="btn btn-outline rounded text-[#3466f6] border border-[#3466f6]" onClick={async e => confirmPost(e, false) }>
             Post
           </button>
-          {paywallSupported && 
-          <div className="pl-2">
-            <button className="btn btn-outline rounded text-[#3466f6] border border-[#3466f6]" onClick={async(e) => confirmPost(e, true)}>
-              Paywall Post
-            </button>
+          {paywallSupported &&
+            <div className="pl-2">
+              <button className="btn btn-outline rounded text-[#3466f6] border border-[#3466f6]" onClick={async e => confirmPost(e, true)}>
+                Paywall Post
+              </button>
             </div>
           }
         </div>
